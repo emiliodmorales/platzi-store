b917651c8b9ffe2c9a4abf8d5dfddd3d92a61551
Froggles <elmsterm@gmail.com>
2026-02-12 13:59:32 -0500

    refactor component file structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from "./Layout/App.tsx";
-import Layout from "./Layout/Layout.tsx";
+import App from "./components/Layout/App.tsx";
+import Layout from "./components/Layout/Layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
