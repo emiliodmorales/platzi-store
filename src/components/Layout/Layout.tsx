5bd7d2a0fcebb9691f9257b5a735b69eb2820b2d
Froggles <elmsterm@gmail.com>
2026-02-12 18:10:32 -0500

    replace login link with a logout button when logged in

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,20 @@
 import { NavLink } from "react-router";
+import { useAuth } from "../Auth/AuthContext";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { token, logout } = useAuth();
+
   return (
     <>
       <header>
         <h1>Platzi Store</h1>
         <nav>
           <NavLink to="/products">Products</NavLink>
-          <NavLink to="/login">Login</NavLink>
+          {token === null ? (
+            <NavLink to="/login">Login</NavLink>
+          ) : (
+            <button onClick={logout}>Logout</button>
+          )}
         </nav>
       </header>
       <main>{children}</main>
