b4b5eb38180ced843e421f61d397bc7d8eb97971
Froggles <elmsterm@gmail.com>
2026-02-12 14:03:30 -0500

    show product list

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import type { Product } from "../../types/product";
+import { getProducts } from "../../api/products";
+
+export default function Products() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const trySetProducts = async () => {
+      const products = await getProducts();
+      setProducts(products);
+    };
+    trySetProducts();
+  }, []);
+
+  return (
+    <>
+      <h2>Products</h2>
+      <ul>
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function ProductItem({ product }: { product: Product }) {
+  return <li>{product.title}</li>;
+}
