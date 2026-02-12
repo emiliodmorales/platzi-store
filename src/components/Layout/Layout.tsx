f550eda2e884238068f7299af6bab17a30392641
Froggles <elmsterm@gmail.com>
2026-02-12 14:39:58 -0500

    create nav bar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,15 @@
-export default function Layout({children}: {children: React.ReactNode}) {
-  return <>
-    <header>
-      <h1>Platzi Store</h1>
-    </header>
-    <main>
-      {children}
-    </main>
-  </>
+import { NavLink } from "react-router";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <header>
+        <h1>Platzi Store</h1>
+        <nav>
+          <NavLink to="/products">Products</NavLink>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./components/Layout/App.tsx";
 import Layout from "./components/Layout/Layout.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
