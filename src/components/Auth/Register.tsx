9315793f36e3c579f1c2f2475f9ee46e634ac07d
Froggles <elmsterm@gmail.com>
2026-02-13 10:07:44 -0500

    create registration form

--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -1,3 +1,32 @@
 export default function Register() {
-  return <h2>Register</h2>;
+  return (
+    <>
+      <h2>Register</h2>
+      <form>
+        <label>
+          <p>Email</p>
+          <input type="text" name="email" />
+        </label>
+        <label>
+          <p>Name</p>
+          <input type="text" name="name" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" name="password" />
+        </label>
+        <label>
+          <p>Confirm Password</p>
+          <input type="password" name="confirmPassword" />
+        </label>
+        <label>
+          <p>Avatar</p>
+          <input type="url" name="avatar" />
+        </label>
+        <p>
+          <button>Register</button>
+        </p>
+      </form>
+    </>
+  );
 }
