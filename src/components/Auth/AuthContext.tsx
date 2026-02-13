2ff057855dada66cc9010afacdd2f206de1551d9
Froggles <elmsterm@gmail.com>
2026-02-13 10:02:46 -0500

    create function to fetch profile details

--- src/components/Auth/AuthContext.tsx
+++ src/components/Auth/AuthContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import type { UserProfile } from "../../types/user";
+import { useNavigate } from "react-router";
 
 const API = import.meta.env.VITE_API;
 const ENDPOINT = API + "/api/v1/auth";
@@ -7,15 +9,16 @@ const AuthContext = createContext<any>(null);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [token, setToken] = useState<String | null>(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const access_token = window.sessionStorage.getItem("access_token");
+    const access_token = window.localStorage.getItem("access_token");
     if (access_token) {
       setToken(access_token);
     }
   }, []);
 
-  /** Get login token from API */
+  /** Get access token from API */
   async function login(credentials: { email: string; password: string }) {
     const response = await fetch(ENDPOINT + "/login", {
       method: "POST",
@@ -31,15 +34,36 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     }
 
     setToken(result.access_token);
-    window.sessionStorage.setItem("access_token", result.access_token);
+    window.localStorage.setItem("access_token", result.access_token);
   }
 
   const logout = () => {
     setToken(null);
-    window.sessionStorage.removeItem("access_token");
+    window.localStorage.removeItem("access_token");
+    navigate("/");
   };
 
-  const value = { token, login, logout };
+  /** Get profile details from API */
+  async function getProfile() {
+    const response = await fetch(ENDPOINT + "/profile", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw Error(result.message);
+    }
+
+    const profile: UserProfile = result;
+
+    return profile;
+  }
+
+  const value = { token, login, logout, getProfile };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
